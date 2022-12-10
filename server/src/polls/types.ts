0a2050e6f24bd6b059af84e6362a319df535e3b9
Jerome <cheonjh4572@gmail.com>
2022-12-10 18:01:46 +0900

    Add poll fields struct type

---
+++ server/src/polls/types.ts
@@ -0,0 +1,15 @@
+export type CreatePollFields = {
+  topic: string;
+  votesPerVoter: number;
+  name: string;
+};
+
+export type JoinPollFields = {
+  pollID: string;
+  name: string;
+};
+export type RejoinPollFields = {
+  pollID: string;
+  userID: string;
+  name: string;
+};
