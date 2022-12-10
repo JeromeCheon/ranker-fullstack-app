6d70614909d0020a17f8366df895a6c6e4f786cd
Jerome <cheonjh4572@gmail.com>
2022-12-10 17:22:57 +0900

    Add poll module, controller, and POST body DTOs

---
+++ server/src/polls/polls.controller.ts
@@ -0,0 +1,22 @@
+import { CreatePollDto, JoinPollDto } from './dtos';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+
+@Controller('polls')
+export class PollsController {
+  @Post()
+  async create(@Body() createPollDto: CreatePollDto) {
+    Logger.log('In create!');
+    return createPollDto;
+  }
+
+  @Post('/join')
+  async join(@Body() joinPollDto: JoinPollDto) {
+    Logger.log('In join!');
+    return joinPollDto;
+  }
+
+  @Post('/rejoin')
+  async rejoin() {
+    Logger.log('In rejoin!');
+  }
+}
