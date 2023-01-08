496b16aca2ef60af1520610833e218c97c019d27
Jerome <cheonjh4572@gmail.com>
2023-01-08 23:39:58 +0900

    Update redis module with asyncMoOptions consisting of factory callback for provider

--- server/src/redis.module.ts
+++ server/src/redis.module.ts
@@ -1,13 +1,51 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import {
+  DynamicModule,
+  FactoryProvider,
+  Module,
+  ModuleMetadata,
+} from '@nestjs/common';
+import IORedis, { Redis, RedisOptions } from 'ioredis';
+
+export const IORedisKey = 'IORedis';
+
+/* JWT token github repo 참고 */
+type RedisModuleOptions = {
+  connectionOptions: RedisOptions;
+  onClientReady?: (client: Redis) => void; // optional callback function
+};
+
+type RedisAsyncModuleOptions = {
+  useFactory: (
+    ...args: any[]
+  ) => Promise<RedisModuleOptions> | RedisModuleOptions;
+} & Pick<ModuleMetadata, 'imports'> & // ModuleMetadata 타입에서 imports 필드 픽
+  Pick<FactoryProvider, 'inject'>;
 
 @Module({})
 export class RedisModule {
-  static async registerAsync(): Promise<DynamicModule> {
+  static async registerAsync({
+    useFactory,
+    imports,
+    inject, // 이것들은 위의 Pick을 통한 Union type
+  }: RedisAsyncModuleOptions): Promise<DynamicModule> {
+    const redisProvider = {
+      provide: IORedisKey,
+      useFactory: async (...args) => {
+        const { connectionOptions, onClientReady } = await useFactory(...args);
+
+        const client = await new IORedis(connectionOptions);
+        onClientReady(client);
+
+        return client;
+      },
+      inject,
+    };
+
     return {
       module: RedisModule,
-      imports: [],
-      providers: [],
-      exports: [],
+      imports,
+      providers: [redisProvider],
+      exports: [redisProvider],
     };
   }
 }
