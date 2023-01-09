d12aad7b11940857e236c56ba6cb4fffa97750e3
Jerome <cheonjh4572@gmail.com>
2023-01-10 00:44:04 +0900

    Update polls.repos by adding CRUD method with redis client methods

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -1,4 +1,11 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Poll } from './../../../shared/poll-types';
+import { AddParticipantData, CreatePollData } from './types';
+import {
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
 import { IORedisKey } from 'src/redis.module';
@@ -14,5 +21,111 @@ export class PollsRepository {
   ) {
     // 여기 IORedisKey는 RedisProvider 정의에서 provide 해준 것
     this.ttl = configService.get('POLL_DURATION');
+  }
+
+  async createPoll({
+    votesPerVoter,
+    topic,
+    pollID,
+    userID,
+  }: CreatePollData): Promise<Poll> {
+    const initialPoll = {
+      id: pollID,
+      topic,
+      votesPerVoter,
+      participants: {},
+      adminID: userID,
+    };
+
+    this.logger.log(
+      `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${
+        this.ttl
+      }`,
+    );
+
+    const key = `polls:${pollID}`;
+
+    try {
+      await this.redisClient
+        .multi([
+          ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)],
+          ['expire', key, this.ttl],
+        ])
+        .exec();
+      return initialPoll;
+    } catch (e) {
+      this.logger.error(
+        `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
+  async getPoll(pollID: string): Promise<Poll> {
+    this.logger.log(`Attempting to get poll with: ${pollID}`);
+
+    const key = `polls:${pollID}`;
+
+    try {
+      const currentPoll = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      );
+
+      this.logger.verbose(currentPoll);
+
+      // if (currentPoll?.hasStarted) {
+      //   throw new BadRequestException('The poll has already started');
+      // }
+
+      return JSON.parse(currentPoll);
+    } catch (e) {
+      this.logger.error(`Failed to get pollID ${pollID}`);
+      throw e;
+    }
+  }
+
+  async addParticipant({
+    pollID,
+    userID,
+    name,
+  }: AddParticipantData): Promise<Poll> {
+    this.logger.log(
+      `Attempting to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+    );
+
+    const key = `polls:${pollID}`;
+    // 여기 participantsPath 경로에서 dot 붙은 이유는 poll-types의 interface에서 string key로 만들었기 때문
+    // 그리고 redis의 특성으로 이 key가 dot 기준 경로의 일부가 될 것
+    const participantPath = `.participants.${userID}`;
+
+    try {
+      await this.redisClient.send_command(
+        'JSON.SET',
+        key,
+        participantPath,
+        JSON.stringify(name),
+      );
+
+      const pollJSON = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      );
+
+      const poll = JSON.parse(pollJSON) as Poll;
+
+      this.logger.debug(
+        `Current Participants for pollID: ${pollID}:`,
+        poll.participants,
+      );
+
+      return poll;
+    } catch (e) {
+      this.logger.error(
+        `Failed to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+      );
+      throw e;
+    }
   }
 }
