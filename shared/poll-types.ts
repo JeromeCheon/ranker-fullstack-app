31b0818d6fe3650a6a0022525473242e5e1b6349
Jerome <cheonjh4572@gmail.com>
2023-01-10 00:21:23 +0900

    Add polls type interfaces in shared dir

---
+++ shared/poll-types.ts
@@ -0,0 +1,18 @@
+export interface Participants {
+	[participantID: string]: string;
+}
+
+export interface Poll {
+	id: string;
+	topic: string;
+	votesPerVoter: number;
+	participants: Participants;
+	adminID: string;
+	// nominations: Nominations;
+	// rankings: Rankings;
+	// results: Results;
+	// hasStarted: boolean;
+}
+
+// 자, 이렇게 package.json, tsconfig, poll-types 까지 생성해줬으면
+// 얘네를 export 시켜줄 index 파일이 필요해;
