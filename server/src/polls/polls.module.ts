d7ad0c3539e10ce5a900ffe85ba32a9b50fc3f01
Jerome <cheonjh4572@gmail.com>
2023-01-10 00:15:24 +0900

    Add polls repository by injecting redis client then import into polls module

--- server/src/polls/polls.module.ts
+++ server/src/polls/polls.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PollsController } from './polls.controller';
 import { PollsService } from './polls.service';
+import { PollsRepository } from './polls.repository';
 
 @Module({
   imports: [ConfigModule, redisModule],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollsRepository],
 })
 export class PollsModule {}
