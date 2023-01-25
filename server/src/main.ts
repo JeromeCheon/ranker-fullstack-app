f92f8caa8a782765eafdf96f6bc69fa9a3043b0e
Jerome <cheonjh4572@gmail.com>
2023-01-25 23:29:04 +0900

    Add custom socketIOAdapter to deal with client port considering of CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,4 @@
+import { SocketIOAdapter } from './socket-io-adapter';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
@@ -9,13 +10,14 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = parseInt(configService.get('PORT'));
-  const clientPort = parseInt(configService.get('CLIENT_PORT'));
+  const clientPort = parseInt(configService.get('CLIENT_PORT')); // this client port doesn't apply to WS directly
   app.enableCors({
     origin: [
       `http://localhost:${clientPort}`,
       new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
     ],
   });
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
   await app.listen(port);
 
   logger.log(`Server running on port ${port}`);
--- server/src/polls/polls.gateway.ts
+++ server/src/polls/polls.gateway.ts
@@ -4,6 +4,9 @@ import { PollsService } from './polls.service';
 
 @WebSocketGateway({
   namespace: 'polls',
+  // cors: {
+  //   origin: [], // 근데 port를 이렇게 일일이 하드코딩 할수는 없는 일. 해결하기 위해 IoAdapter 쓰자
+  // },
 })
 export class PollsGateway implements OnGatewayInit {
   private readonly logger = new Logger(PollsGateway.name);
