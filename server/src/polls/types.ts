790a79166eef27e7b1ae1dd6993490a7fd6824f0
Jerome <cheonjh4572@gmail.com>
2023-02-11 17:33:40 +0900

    Update service by connecting repository add and remove participant methods

--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -12,11 +12,23 @@ export type JoinPollFields = {
   pollID: string;
   name: string;
 };
+
 export type RejoinPollFields = {
   pollID: string;
   userID: string;
   name: string;
 };
+
+export interface AddParticipantFields {
+  pollID: string;
+  userID: string;
+  name: string;
+}
+
+export interface RemoveParticipantData {
+  pollID: string;
+  userID: string;
+}
 
 // repository types
 export type CreatePollData = {
