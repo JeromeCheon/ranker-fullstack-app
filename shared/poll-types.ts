0f524a95a746a27097ce78caa63530b8d3bbae44
Dev. Jerome <cheonjh4572@gmail.com>
2023-02-13 23:52:44 +0900

    Add Nomination type and NominationData fields

--- shared/poll-types.ts
+++ shared/poll-types.ts
@@ -1,18 +1,27 @@
 export interface Participants {
-	[participantID: string]: string;
+  [participantID: string]: string;
 }
 
-export interface Poll {
-	id: string;
-	topic: string;
-	votesPerVoter: number;
-	participants: Participants;
-	adminID: string;
-	// nominations: Nominations;
-	// rankings: Rankings;
-	// results: Results;
-	hasStarted: boolean;
-}
+export type Nomination = {
+  userID: string;
+  text: string;
+};
+
+export type Nominations = {
+  [nominationID: string]: Nomination;
+};
+
+export type Poll = {
+  id: string;
+  topic: string;
+  votesPerVoter: number;
+  participants: Participants;
+  adminID: string;
+  nominations: Nominations;
+  // rankings: Rankings;
+  // results: Results;
+  hasStarted: boolean;
+};
 
 // 자, 이렇게 package.json, tsconfig, poll-types 까지 생성해줬으면
 // 얘네를 export 시켜줄 index 파일이 필요해;
