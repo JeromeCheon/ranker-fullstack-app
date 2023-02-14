aa3865d1cd126da96a45eaf08add65f118b6c25c
Dev. Jerome <cheonjh4572@gmail.com>
2023-02-14 23:44:57 +0900

    Add add/remove Nomination methods and dto in service and gateway with ws room emit

--- server/src/polls/polls.service.ts
+++ server/src/polls/polls.service.ts
@@ -1,9 +1,11 @@
+import { createNominationID } from './../ids';
 import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Poll } from 'shared';
 import { createPollID, createUserID } from 'src/ids';
 import { PollsRepository } from './polls.repository';
 import {
+  AddNominationFields,
   AddParticipantFields,
   CreatePollFields,
   JoinPollFields,
@@ -106,5 +108,24 @@ export class PollsService {
 
   async getPoll(pollID: string): Promise<Poll> {
     return this.pollsRepository.getPoll(pollID);
+  }
+
+  async addNomination({
+    pollID,
+    userID,
+    text,
+  }: AddNominationFields): Promise<Poll> {
+    return this.pollsRepository.addNomination({
+      pollID,
+      nominationID: createNominationID(),
+      nomination: {
+        userID,
+        text,
+      },
+    });
+  }
+
+  async removeNomination(pollID: string, nominationID: string): Promise<Poll> {
+    return this.pollsRepository.removeNomination(pollID, nominationID);
   }
 }
